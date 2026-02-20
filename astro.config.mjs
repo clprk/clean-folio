e6e803ba644c09dd2999c9036f97cfc0fd045859
ciel park <cielcpark@gmail.com>
2026-02-20 14:51:49 -0500

    redeploy without adapter lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,24 @@ import tailwindcss from '@tailwindcss/vite';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 
-import vercel from '@astrojs/vercel';
+//import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   integrations: [
     sanity({
-      projectId: "rnk0k1nk",
-      dataset: "production",
+      projectId: 'rnk0k1nk',
+      dataset: 'production',
       useCdn: false, // for static builds
-      // studioBasePath: '/studio' 
+      // studioBasePath: '/studio'
     }),
     react(),
   ],
 
-  adapter: vercel()
+  // adapter: vercel()
+  output: 'static',
 });
